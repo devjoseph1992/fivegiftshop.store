98aa38b01d76927e70fe3eef80a50b8f27afee98
John Joseph Florina <devjosephflorina@gmail.com>
2021-03-03 17:43:00 +0800

    configure material ui || adding navbar components

---
+++ component/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <>
+      <h1>Hello Navbar</h1>
+    </>
+  )
+}
+
+export default Navbar
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,39 @@
-import '../styles/globals.css'
+import React, { useEffect }from 'react';
+import PropTypes from 'prop-types';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../src/theme';
+import Head from 'next/head'
+import Layout from '../component/Layout';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
